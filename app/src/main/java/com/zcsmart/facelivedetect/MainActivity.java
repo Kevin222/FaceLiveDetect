d49aee37ef5250c6c578abe4ece23e26c8e04118
曹凯 <ck122@zcsmart.com>
2017-08-03 15:34:43 +0800

    init

---
+++ app/src/main/java/com/zcsmart/facelivedetect/MainActivity.java
@@ -0,0 +1,30 @@
+package com.zcsmart.facelivedetect;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.hisign.CTID.facelivedetection.CTIDLiveDetectActivity;
+
+/**
+ * Created by caokai on 2017/8/2.
+ */
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+
+    public void clickButton(View view){
+        switch (view.getId()){
+            case R.id.btn:
+                startActivity(new Intent(this, CTIDLiveDetectActivity.class));
+                break;
+        }
+    }
+}
